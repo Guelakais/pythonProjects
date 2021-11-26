b9603a0d5d21854145ab6a35c2e0c24e5dba6ba3
Guelakais <koroyeldiores@gmail.com>
2021-11-26 17:47:33 +0100

    better audiobookmaker

--- AudBookMkr.py
+++ AudBookMkr.py
@@ -11,21 +11,19 @@ def inputManager():
         userInput = input("Hello, I'm here to help you to convert a text files to audio files. \n Please tell me the path of your File: ")
         my_file = Path(userInput)
         if my_file.is_file():
-            userInput2 = input("Where do you want to save the file? [/path/Filename] \n Disclaimer: Ending is always mp3 ")
-            end = ".mp3"
-            realOutput = userInput2 + end
+            userInput2 = input("Where do you want to save the file? [/path/Filename] \n Disclaimer: Ending is always mp3 "
+            realOutput = userInput2 + ".mp3"
             AudBookMkr(userInput, realOutput)
             break
         else:
             print("no such path found, please try again")
 
 def AudBookMkr(file, audio):
-    fh = open(file, "r")
-    myText = fh.read().replace("\n", " ")
-    # Language we want to use 
-    language = 'en'
-    output = gTTS(text=myText, lang=language, slow=False)
-    output.save(audio)
-    fh.close()
+    with open(file, "r") as fh:
+        myText = fh.read().replace("\n", " ")
+        # Language we want to use 
+        language = 'en'
+        output = gTTS(text=myText, lang=language, slow=False)
+        output.save(audio)
 
 inputManager()
